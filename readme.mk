a40d68c91eb75e055510754783b7901406bcb769
dzamalez <diego.zamora193@gmail.com>
2021-06-18 22:22:18 -0500

    final product

---
+++ readme.mk
@@ -0,0 +1,7 @@
+This project was inspired by my followers on instagram who submitted quotes. I think took their quotes and randomized their quotes and 
+authors to create a "wacky" quote. 
+
+The structure of my code was inspired from other students fro the codecademy full stack engineer course. From there I inserted all the quotes
+submitted from my followers. Upon clicking the button on the html page, the user then gets a randomized motivational wuote for the day.
+
+Pelase emjoy, this is my first major coding prject.
